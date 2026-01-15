f3065bf84c5f7be7a8eabfd4a1f6d746acfb98eb
Crazy Rich <zorveex1@gmail.com>
2026-01-15 13:41:18 +0700

    Create Member.lua

---
+++ Member.lua
@@ -0,0 +1,14 @@
+--[[
+
+  ________         _______       ____     ____          ____   ____
+ |___    /  ____  |   __   \     \   \   /   /________ \   \ /   /
+    /   /  /    \ |  |__)  |      \   \ /   /|   _____| \   V   /
+   /   /  /  /\  \|        /       \   V   / |  |__     /   ^   \
+  /   /___\  \/  /|   |\  \         \     /  |   __|_  /   / \   \
+ /________|\____/ |___| \__\         \___/   |_______|/___/   \___\
+ 
+ owner ZorVex
+
+]]
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Galaxy-Rich/Script/refs/heads/main/ZorVex-Member.lua'))()
